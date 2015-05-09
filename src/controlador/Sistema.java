f3e4d167ab7865cd74e5120b4be765345d0f772c
anocetti010 <anocetti010@ARND2001.soa.ad.pwcinternal.com>
2015-05-09 17:13:45 -0300

    Se agrego la clase sistema y la base del método buscarEdiciones

---
+++ src/controlador/Sistema.java
@@ -0,0 +1,32 @@
+package controlador;
+
+import java.util.Date;
+import java.util.Vector;
+
+import modelo.Edicion;
+import modelo.Publicacion;
+import modelo.Vendedor;
+
+public class Sistema {
+	
+	private Vector<Vendedor> vendedores;
+	private Vector<Edicion> ediciones;
+	
+	public Sistema(Vector<Vendedor> vendedores, Vector<Publicacion> publicaciones) {
+		super();
+		this.vendedores = new Vector<Vendedor>();
+		this.ediciones = new Vector<Edicion>();
+	}
+
+	public Vector<Edicion> buscarEdiciones (Date fecha){
+		Vector<Edicion> edicionesBuscadas = new Vector<Edicion>();
+		for(Edicion edicion: ediciones){
+			if (edicion.getFechaSalida().equals(fecha)){
+				edicionesBuscadas.add(edicion);
+			}
+		}
+		return edicionesBuscadas;
+	}
+	
+}
+
