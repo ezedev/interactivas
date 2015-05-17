116e32cb26a753feac2e9a3af0d397161603fbb0
mauriciozuain <mauriciozuain@192.168.0.101>
2015-05-17 14:53:16 -0300

    Modificado el metodo obtenerUltimas3 de la clase PublicacionDiario.

--- src/modelo/PublicacionDiario.java
+++ src/modelo/PublicacionDiario.java
@@ -16,11 +16,12 @@ public class PublicacionDiario extends Publicacion{
 		super(codigo, titulo, editor, tema, subtema, publico, idioma,paisDeOrigen);
 	}
 	
-	@SuppressWarnings("deprecation")
 	public Vector<Edicion> obtenerUltimas3() {
 		Vector <Edicion> e = getEdiciones();
 		Vector <Edicion> ultimas3 = new Vector <Edicion> ();
 		Calendar hoy = Calendar.getInstance();
+		hoy.add(Calendar.DAY_OF_YEAR, 1);
+		hoy.add(Calendar.DAY_OF_YEAR, -7);
 		//busco las ultimas edicones segun el dia (pueden ser 1,2 o 3)
 			for (int i=e.size()-1; i >= 0 && ultimas3.size() < 3; i--){
 				if (hoy.equals(e.elementAt(i).getFechaSalida())){
